c8222689885646e27b395f79ddb154e1a2412bd7
yasmin <yasmin@lit.ifce.edu.br>
2022-03-10 11:22:57 -0300

    changing git commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/weather/:cityId', to: 'weather#send_weather_for_twitter', as: :weather
 end
